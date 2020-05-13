bf3c4796e4f00be1f9a347e005c3289c54cf05f5
Uno <duyduong@ucla.edu>
2020-05-13 19:27:09 +0000

    Db backup 5/13

---
+++ db.sql
@@ -0,0 +1,137 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.7 (Debian 11.7-2.pgdg90+1)
+-- Dumped by pg_dump version 11.7 (Debian 11.7-2.pgdg90+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    user_id character varying(20) NOT NULL,
+    afk_message character varying(255)
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (user_id, afk_message) FROM stdin;
+149796032480411648	<a:sorcthink:432143041583775755> what's your emergency
+158620282741850112	https://imgur.com/jBIRPh7
+292899571196100608	Please try again later <:kawncern:360107173038129164>  https://78.media.tumblr.com/a8823e736414aa5b8c5f8578d6565ec3/tumblr_opy59iHeFc1vnq1cro1_400.gif
+127058056650555393	\N
+91859502151774208	\N
+176430249246982144	https://i.imgur.com/ec1iSaP.gifv
+192662764609732608	https://cdn.discordapp.com/attachments/498414970179158016/560151629228277773/312312.png
+238987416113774594	https://media.discordapp.net/attachments/306823331720593409/470221043487735829/DN_2018-07-21_20-52-18_Sat.jpg?width=989&height=633
+331419979914280961	message https://giphy.com/gifs/etAwbBMOO3K6ny5De5
+151569840987963392	it's a sign
+170947680783171584	\N
+371494567079444480	AFK
+387063646498455569	\N
+210396362284466178	🔪 <:blobglare:358778370533621780>
+435038630147522561	If I'm AFK call 911.
+92669124332785664	\N
+177303460469800960	https://media.giphy.com/media/XBWNNWysXN80AJvmwz/giphy.gif
+203203464388411392	https://gfycat.com/spitefulbadkronosaurus
+115745578259841030	\N
+296290427307425792	https://tenor.com/view/maki-nishikino-blush-love-live-gif-5438490
+212193185982709761	<.>
+549495111248117760	https://imgur.com/2jccsGF
+324199981735084032	DM if really important, not reading tags
+124919896567054337	\N
+250337724001681410	https://i.imgflip.com/36ma1w.gif
+260067890806390784	https://tenor.com/bfIWc.gif
+388927156417331200	AFK FOREVER  ( ͡° ͜ʖ ͡°)
+186268942887550977	https://cdn.discordapp.com/attachments/644173142549463041/648714673616715786/unknown.png
+475222444907560972	Thank for coming my wedding\nhttps://cdn.discordapp.com/attachments/640574595258384423/696357541826920458/DN_2020-04-05_21-41-41_Sun.png
+325582117079613441	https://cdn.discordapp.com/attachments/265815224114741268/632522935697014785/8c30f9ae1b4f8aca1ba0a08aa41aa29ba593182fccf1f4e2a941efebfab3ebc1.png
+553199800049205296	\N
+531922836915224584	https://www.youtube.com/watch?v=N8j6U2z94wA
+344669179829288961	https://i.pinimg.com/originals/6c/f4/a3/6cf4a35ff341044e0ef2acf09f128d5b.gif
+184353047730716672	https://pm1.narvii.com/7408/08a9f1733cff57d7c5398cff7242e05ae13bede9r1-1500-785v2_hq.jpg
+236457905858150400	o(-<
+208782729565700107	https://cdn.discordapp.com/attachments/265816937265758209/687896201248374821/j7bdo.mp4
+100223894438301696	\N
+305330737287004160	https://media.discordapp.net/attachments/464663685634195469/641985268886536193/IMG_20191102_190733.jpg
+423433870571208705	https://media.giphy.com/media/UWzK5hAREuKoJt9Vai/giphy.gif
+286900099123118081	https://media.tenor.com/images/0971bb006399f91fac6f79bd5c191f33/tenor.gif
+300237849268322305	50k lesgo https://cdn.discordapp.com/attachments/623945244991291393/704598405678235711/unknown.png
+504708016659103757	https://cdn.discordapp.com/attachments/265815224114741268/628510323057491968/DN_2019-10-01_16-29-01_Tue_-_Copy.png
+578372583939112960	https://imgur.com/qXBIkP0
+284796716669206538	Ceeeeeeeeeeeeeeeeeeeeeeeb
+404075418275282955	http://imgur.com/gallery/BQX82c0
+252338427301724171	https://cdn.discordapp.com/attachments/244790887509393410/690484798673911868/brutal_dn_memes.jpg
+205419429448646656	Vandar in a nutshell https://cdn.discordapp.com/attachments/653854850324365312/704796452756521002/61-UGbUqCcL._SL1000_.jpg
+239783159485431819	On indefinite hiatus, DM only when urgent ||or incase you are in `B**** D***** SEA PC`|| Thank~
+518984357839503380	https://giphy.com/gifs/dragon-nest-j3JVix5kfRSr57154A
+367840314070925313	https://imgur.com/57o92qh
+149795295738331137	\N
+318059824912924672	https://66.media.tumblr.com/d123a2b16b942e4e1f9a748b48c432a5/tumblr_phexhvO93O1tlb56zo1_400.gif
+282865120747585536	delete
+415005450120069121	https://imgur.com/a/09J5Ewg
+205656607093161984	https://imgur.com/AkFizmx
+436727184770596864	https://cdn.discordapp.com/attachments/543805298238816268/605231770014711828/source.gif
+470764081196826626	https://media.giphy.com/media/cncYuwWToR8smDgwiZ/giphy.gif
+125909927289028608	\N
+328462720070582272	https://cdn.discordapp.com/attachments/118217408853835785/556043716473782272/tenor.gif
+297372683044257793	https://imgur.com/MX6XlAt
+224044342975660034	\N
+229793785972916225	https://cdn.discordapp.com/attachments/409397220459413515/680782146406711309/unknown.png
+220950052925865985	\N
+159317182835064833	\N
+222080822365257728	\N
+250176185768607744	\N
+193450996112949248	https://cdn.discordapp.com/attachments/530738314647437333/663494798618918913/Hi1gif.gif
+597375701628485634	https://imgur.com/Zfn2qk6
+227375126725787648	https://tenor.com/view/arknights-pramanix-fluffy-cute-bite-gif-16873917
+356668402942607364	https://cdn.discordapp.com/attachments/590197224781250608/659475591069827072/IndolentSpiffyBongo-size_restricted.gif
+423784947212484629	https://tenor.com/view/dragon-nest-filthy-frank-trash-meme-know-your-fucking-place-gif-15772338
+169777208259379201	https://cdn.discordapp.com/attachments/436791099483226113/704697386374791168/images.png
+440764140500418572	https://media.giphy.com/media/UTLYdtdfh3MrcqYiZH/giphy.gif
+269005895743176705	https://thumbs.gfycat.com/DependentMiserableArrowcrab-size_restricted.gif
+433478125528154114	popcorn https://cdn.discordapp.com/emojis/707413767637696632.png?v=1
+375497412422139905	https://cdn.discordapp.com/attachments/672702363848540190/684710156348817408/FB_IMG_1566418405233.jpg
+227090785135362048	`DEJA VU!` https://media.giphy.com/media/YrqswRfkK9HbJtkCS5/giphy.gif
+431658444010029077	https://media.discordapp.net/attachments/265815224114741268/688284827899658361/Reina.gif
+474827726910390273	https://media.giphy.com/media/dxaLYAcbxxUqrCRHuc/giphy.gif
+362554131539296257	Always bring alcohol with you, let us fight COVID 19~
+537003337997090836	https://cdn.discordapp.com/attachments/643146811309293608/686790547544866839/ff181028-a5b4-4674-97da-a92184e7b9d5.jpg
+235040810289201156	\N
+223812232339914753	\N
+\.
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (user_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
